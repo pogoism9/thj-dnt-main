5bc65b60c8132d5be1a2199bed6cd62dc7cbe46b
Amber Valderez <amber.valderez@gmail.com>
2025-01-06 23:23:41 -0800

    Add all four bankers

--- src/app/@shared/utils.ts
+++ src/app/@shared/utils.ts
@@ -1,52 +1,60 @@
-import { BankEntry } from "./@models/bank-entry.type";
+import { BankEntry } from './@models/bank-entry.type';
 
 export function outputFileToJson(rawData: string): BankEntry[] {
     // Split the raw data by new lines
-    const lines = rawData.split("\n");
+    const lines = rawData.split('\n');
 
     // Define an array to store the results
     const result: BankEntry[] = [];
 
-    const blacklistedItemIds = [ 
+    const blacklistedItemIds = [
+        9990, // Skin of Milk
+        9991, // Bread Cakes
         17005, // Backpack
         17423, // Adventurer's Pack
-        976012, // Large Bank Box
-    ]
+        18756, // Tattered Note
+        976012, // Large Bank Box,
+    ];
 
-    const blacklistedItemLocations = [
-        'Primary',
-        'Chest',
-    ]
+    const blacklistedItemLocations = ['Primary', 'Secondary', 'Chest'];
 
     // Iterate through each line
-    for (let i = 1; i < lines.length; i++) {  // Skip the first line (headers)
+    for (let i = 1; i < lines.length; i++) {
+        // Skip the first line (headers)
         const line = lines[i].trim();
 
         // Skip empty lines
-        if (line === "") continue;
+        if (line === '') continue;
 
         // Split the line by tabs or spaces
-        const columns = line.split(/\s{2,}|\t/);  // Match any number of spaces or tab
+        const columns = line.split(/\s{2,}|\t/); // Match any number of spaces or tab
 
         // Ensure the line has the expected number of columns
         if (columns.length === 5) {
             const [location, name, id, count, slots] = columns;
-            if (+id === 0 || 
+            if (
+                +id === 0 ||
                 blacklistedItemIds.includes(+id) ||
                 blacklistedItemLocations.includes(location)
-            ) 
+            )
                 continue;
-            
 
-            const bankEntry: BankEntry = {
-                location,
-                name,
-                id: +id,
-                count: +count,
-                slots: +slots
-            };
-
-            result.push(bankEntry);
+            if (result.some((entry) => entry.id === +id)) {
+                const existingEntry = result.find((entry) => entry.id === +id);
+                if (existingEntry) {
+                    existingEntry.count += +count;
+                    continue;
+                }
+            } else {
+                const bankEntry: BankEntry = {
+                    location,
+                    name,
+                    id: +id,
+                    count: +count,
+                    slots: +slots,
+                };
+                result.push(bankEntry);
+            }
         }
     }
     return result;
--- src/app/bank/bank.component.ts
+++ src/app/bank/bank.component.ts
@@ -3,22 +3,40 @@ import { Component } from '@angular/core';
 import { outputFileToJson } from '../@shared/utils';
 import { BankEntry } from '../@shared/@models/bank-entry.type';
 import { CommonModule } from '@angular/common';
-
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'ariza-bank',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatTabsModule
+  ],
   templateUrl: './bank.component.html',
   styleUrl: './bank.component.scss'
 })
 export class BankComponent {
-    public bankData: BankEntry[] = [];
+    
+    public bankData: Map<string, BankEntry[]> = new Map<string, BankEntry[]>();
+
     constructor(private _http: HttpClient) {}
 
     ngOnInit(): void {
-        this._http.get('assets/Dntspells-Inventory.txt', {responseType: 'text'}).subscribe(data => {
-            let bankData: BankEntry[] = outputFileToJson(data);
-            bankData = bankData.sort((a, b) => a.name.localeCompare(b.name));
-            this.bankData = bankData;
+        const targetFiles = [
+            'Dntbank-Inventory.txt',
+            'Dntdoze-Inventory.txt',
+            'Dntepics-Inventory.txt',
+            'Dntspells-Inventory.txt',
+        ];
+
+        targetFiles.forEach(file => {
+            this._http.get(`assets/${file}`, {responseType: 'text'}).subscribe(data => {
+                let processedData: BankEntry[] = outputFileToJson(data);
+
+                processedData = processedData.sort((a, b) => a.name.localeCompare(b.name));
+                const name = file.split('-')[0].substring(3);
+                this.bankData.set(name, processedData);
+            });
         });
     }
 }
