5e99aa4d425837858e40c833c5091a119161c480
Amber Valderez <amber.valderez@gmail.com>
2025-03-26 22:45:34 -0700

    Fix display before initialization

--- src/app/bank/bank.component.ts
+++ src/app/bank/bank.component.ts
@@ -42,21 +42,23 @@ export class BankComponent {
         this.item$ = collectionData(itemCollection);
         this._interval = setInterval(() => {
             this._date = new Date();
-            const diffInMilliseconds = this._date.getTime() - this.lastModified!.getTime();
-            const diffInMinutes = Math.floor(diffInMilliseconds / (1000 * 60));
-            const diffInHours = Math.floor(diffInMilliseconds / (1000 * 60 * 60));
-            const diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
-            const parts: string[] = [];
-            if (diffInDays > 0) {
-                parts.push(`${diffInDays} days`);
+            if (this.lastModified) {
+                const diffInMilliseconds = this._date.getTime() - this.lastModified!.getTime();
+                const diffInMinutes = Math.floor(diffInMilliseconds / (1000 * 60));
+                const diffInHours = Math.floor(diffInMilliseconds / (1000 * 60 * 60));
+                const diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+                const parts: string[] = [];
+                if (diffInDays > 0) {
+                    parts.push(`${diffInDays} days`);
+                }
+                if (diffInHours % 24 > 0) {
+                    parts.push(`${diffInHours % 24} hours`);
+                }
+                if (diffInMinutes % 60 > 0) {
+                    parts.push(`${diffInMinutes % 60} minutes`);
+                }
+                this.lastModifiedDisplay = `${parts.join(', ')} ago`;
             }
-            if (diffInHours % 24 > 0) {
-                parts.push(`${diffInHours % 24} hours`);
-            }
-            if (diffInMinutes % 60 > 0) {
-                parts.push(`${diffInMinutes % 60} minutes`);
-            }
-            this.lastModifiedDisplay = `${parts.join(', ')} ago`;
         }, 1000);
     }
 
