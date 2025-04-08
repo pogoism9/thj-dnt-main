9aa92eec4cff0b66e19095d5d198af80fc944b2a
Amber Valderez <amber.valderez@gmail.com>
2025-04-07 22:19:21 -0700

    Change default route to bank

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { BankComponent } from './bank/bank.component';
 import { UploadComponent } from './upload/upload.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'bank', pathMatch: 'full' },
     { path: 'bank', component: BankComponent },
     { path: 'upload', component: UploadComponent },
 ];
